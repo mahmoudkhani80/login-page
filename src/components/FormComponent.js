14c2b50b472d635f3aa0b66d47da8af004962845
unknown <ali.mahmoudkhani.80@gmail.com>
2023-05-08 15:26:16 +0430

    move password input from passwordform to formcomponents

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -1,4 +1,26 @@
+import React, { useState } from "react";
+
 function Email() {
+  const [email, setEmail] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
+  function handleEmailChange(e) {
+    const value = e.target.value;
+    setEmail(value);
+
+    if (!isValidEmail(value)) {
+      setErrorMsg("Please enter a valid email address.");
+    } else {
+      setErrorMsg("");
+    }
+  }
+
+  function isValidEmail(value) {
+    // Use a regular expression (regex) to validate the email input
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    return emailRegex.test(value);
+  }
+
   return (
     <div>
       <div className="input-layout">
@@ -7,12 +29,23 @@ function Email() {
         </label>
         <input
           type="email"
-          name="email"
+          name={email}
+          onChange={handleEmailChange}
           className="border-for-inputs"
         />
+        <p className="error">{errorMsg}</p>
+        <div className="input-layout">
+          <label>
+            <strong className="grey-text">Password</strong>
+          </label>
+          <input
+            type="password"
+            name="password"
+            className="border-for-inputs"
+          />
+        </div>
       </div>
     </div>
   );
 }
-
 export default Email;
