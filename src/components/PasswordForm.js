91978858f116fab4f82670552f27a9d4aa1cb060
unknown <ali.mahmoudkhani.80@gmail.com>
2023-05-09 14:26:47 +0430

    password input validation added

--- src/components/PasswordForm.js
+++ src/components/PasswordForm.js
@@ -1,34 +1,33 @@
 import React, { useState } from "react";
-function PassWord() {
-  const [password, setPassWord] = useState("");
-  const [PassWorderrorMsg, setPassWordErrorMsg] = useState("");
 
-  function handlePassWordChange(p) {
-    const pass = p.target.pass;
-    setPassWord(pass);
+function PasswordInput() {
+  const [password, setPassword] = useState("");
+  const [isValid, setIsValid] = useState(false);
 
-    if (!isValidPassWord(pass)) {
-      setPassWordErrorMsg("Please enter a valid password.");
-    } else {
-      setPassWordErrorMsg("");
-    }
-  }
+  const handleChange = (event) => {
+    const regex = /^(?=.*[a-zA-Z]).{8,}$/; // regex that matches at least 8 characters combined with numbers
+    const passwordValue = event.target.value;
 
-  function isValidPassWord(pass) {
-    // Use a regular expression (regex) to validate the PassWord input
-    const PassWordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})$/;
-    return PassWordRegex.test(pass);
-  }
+    setPassword(passwordValue);
+    setIsValid(regex.test(passwordValue));
+  };
 
   return (
     <div className="input-layout">
       <label>
         <strong className="grey-text">Password</strong>
       </label>
-      <input type="password" name={password} onChange={handlePassWordChange} link className="border-for-inputs" />
-      <p className="error">{PassWorderrorMsg}</p>
+      <input
+        type="password"
+        onChange={handleChange}
+        value={password}
+        className="border-for-inputs"
+      />
+      {isValid ? null : (
+        <p className="error">Password must be at least 8 characters.</p>
+      )}
     </div>
   );
 }
 
-export default PassWord;
+export default PasswordInput;
