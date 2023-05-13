f74458ec34184446a5df4ded280b43cd6f534f1d
unknown <ali.mahmoudkhani.80@gmail.com>
2023-05-14 00:02:43 +0430

    make validation for RadioButton inputs

--- src/components/RadioButtons.js
+++ src/components/RadioButtons.js
@@ -2,23 +2,34 @@ import React, { useState } from "react";
 import SubmitButton from "./Submit";
 function Radio(props) {
   const [option, setOption] = useState(null);
+  const [OptionerrorMsg, setOptionErrorMsg] = useState('');
+  const errorMsg = props.error;
+  function handleOption(event) {
+    setOption(event.target.value);
+    props.option(event.target.value);
+  }
+  // function isValid(option) {
+  //   if (option === null) {
+  //     setOptionErrorMsg("please select one option");
+  //     return false;
+  //   } else {
+  //     setOptionErrorMsg('')
+  //     return true;
+  //   }
+  // }
 
-  const handleOption = (event) => {
-    const value = event.target.value;
-    setOption(value);
-  };
-  // <SubmitButton option={option}/> 
+  // <SubmitButton option={option}/>
   // console.log(option);
+
   return (
     <div>
-      
       <p className="grey-text">Looking for?</p>
       <div className="radio-container">
         <div className="radio-buttons grid-for-radio">
           <input
             type="radio"
             name="look"
-            value={props.project}
+            value="project"
             checked={option === "project"}
             onChange={handleOption}
           />
@@ -28,12 +39,13 @@ function Radio(props) {
           <input
             type="radio"
             name="look"
-            value={props.design}
+            value="design"
             checked={option === "design"}
             onChange={handleOption}
           />
           <label>Design</label>
         </div>
+        {errorMsg && <p className="error">{errorMsg}</p>}
       </div>
     </div>
   );
