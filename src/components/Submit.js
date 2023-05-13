26d94d26be06cab14180aa7be6d16ceab524ca10
unknown <ali.mahmoudkhani.80@gmail.com>
2023-05-14 00:04:26 +0430

    radiobutton & email validation added

--- src/components/Submit.js
+++ src/components/Submit.js
@@ -1,36 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import EmailForm from "./EmailForm";
 import PasswordForm from "./PasswordForm";
 import TagsInput from "./TagsInput";
 import RadioButtons from "./RadioButtons";
-import Radio from "./RadioButtons";
 
-function SubmitButton(props) {
+function SubmitButton() {
+  const [option, setOption] = useState(null);
+  const [OptionerrorMsg, setOptionErrorMsg] = useState("");
+  const [Email, setEmail] = useState("");
+  const [EmailerrorMsg, setEmailErrorMsg] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
-    // if (props.option === undefined) {
-    //   console.log(props.option);
-    // } else {
-    //   console.log("failed");
-    // }
-    // <RadioButtons />
-    // <EmailForm />
-    // <PasswordForm />
-    // <TagsInput />
+    // radio buttons
+    if (option === "project" || "design") {
+      setOptionErrorMsg("");
+      // console.log(option);
+    }
+    if (option === null) {
+      setOptionErrorMsg("please select one option.");
+      // console.log(option);
+    }
+    // end of radio buttons
+    // email
+
+    if (!isValidEmail(Email)) {
+      // console.log(Email);
+      setEmailErrorMsg("please enter email address");
+      return;
+    }
+    if (isValidEmail(Email)) {
+      // console.log(Email);
+      setEmailErrorMsg("");
+    }
+    // end of email
   }
+  // console.log(setEmailErrorMsg);
+  function handleOption(option) {
+    setOption(option);
+  }
+  function handleEmail(event) {
+    setEmail(event.target.value);
+  }
+  const isValidEmail = (Email) => {
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    return emailRegex.test(Email);
+  };
 
   return (
     <>
-    
-      {/* <Form/> */}
-      {/* radio buttons */}
-      <RadioButtons project='project' design='design'/>
-      {/* <Email Input /> */}
-      {/* <EmailForm /> */}
-      {/* <Password Input /> */}
-      {/* <PasswordForm /> */}
-      {/* skills input */}
-      {/* <TagsInput /> */}
+      <form onSubmit={handleSubmit}>
+        {/* radio buttons */}
+        <RadioButtons error={OptionerrorMsg} option={handleOption} />
+        {/* <Email Input /> */}
+        <EmailForm error={EmailerrorMsg} onChange={handleEmail} />
+        {/* <Password Input /> */}
+        <PasswordForm />
+        {/* skills input */}
+        <TagsInput />
+      </form>
       <div>
         <button
           type="submit"
