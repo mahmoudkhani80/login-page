ca961b57071d0217032856685cdc412da694c7ee
unknown <ali.mahmoudkhani.80@gmail.com>
2023-05-14 00:03:42 +0430

    made validation for email input

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,25 +1,26 @@
 import React, { useState } from "react";
 
-function Email() {
+function Email(props) {
   const [email, setEmail] = useState('');
   const [errorMsg, setErrorMsg] = useState('');
+  const error = props.error;
+  // function handleEmailChange(e) {
+  //   const value = e.target.value;
+  //   setEmail(value);
 
-  function handleEmailChange(e) {
-    const value = e.target.value;
-    setEmail(value);
+  // }
+    // if (!isValidEmail(value)) {
+    //   setErrorMsg('Please enter a valid email address.');
+    // } else {
+    //   setErrorMsg('');
+    // }
+  
 
-    if (!isValidEmail(value)) {
-      setErrorMsg('Please enter a valid email address.');
-    } else {
-      setErrorMsg('');
-    }
-  }
-
-  function isValidEmail(value) {
-    // Use a regular expression (regex) to validate the email input
-    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-    return emailRegex.test(value);
-  }
+  // function isValidEmail(value) {
+  //   // Use a regular expression (regex) to validate the email input
+  //   const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  //   return emailRegex.test(value);
+  // }
 
   return (
     <div>
@@ -27,8 +28,8 @@ function Email() {
         <label>
           <strong className="grey-text">Email</strong>
         </label>
-        <input type="email" name="email" onChange={handleEmailChange} className="border-for-inputs" />
-        {errorMsg && <p className="error">{errorMsg}</p>}
+        <input type="email" name="email" onChange={props.onChange} className="border-for-inputs" />
+        {error && <p className="error">{error}</p>}
       </div>
     </div>
   );
